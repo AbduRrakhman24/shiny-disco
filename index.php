c803ec9b61ad8ad04d668a0148b9586a9d6b7462
AbduRrkhman24 <ahmadov.1999@bk.ru>
2023-10-14 14:36:15 +0300

    first

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 25;
+
+$b = 5;
+
+$c = $a + $b;
+$d = $a * $b;
+$e = $a / $b;
+
+
+$name = ' Abdurrakman ';
+
+$age = ' 24.';
+
+echo  $c . ' ' . $d . ' ' . $e . '<br>' . 'Hello' . $name . 'your age is' . $age;
