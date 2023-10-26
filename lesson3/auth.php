c4f6e836b1d002b29b5f2cda76c3eea264d33a60
AbduRrkhman24 <ahmadov.1999@bk.ru>
2023-10-26 22:53:12 +0300

    sd

--- lesson3/auth.php
+++ lesson3/auth.php
@@ -1,21 +1,27 @@
 <?php
 
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$date = $_POST['birthDate'];
-//$men = $_POST['Men'];
+
+
+
 //$women = $_POST['Women'];
 
 
-echo 'Имя - ' . $name . '<br/>';
-echo 'Фамилия - ' . $surname . '<br/>';
-echo 'Дата рождения - ' . $date . '<br/>';
+if (isset($_POST['name'], $_POST['surname'], $_POST['birthDate']) && $_POST['name'] !== '' && $_POST['surname'] !== '' && $_POST['birthDate'] !== '')
+{
+    echo 'Имя - ' . $_POST['name'] . '<br/>';
+    echo 'Фамилия - ' . $_POST['surname'] . '<br/>';
+    echo 'Дата рождения - ' . $_POST['birthDate'] . '<br/>';
+}
+
+if (isset($_POST['Men']) && $_POST['Men'] === 'on' )
+{
+    echo 'Пол - Мужской';
+} 
+    else if(isset($_POST['Women']) && $_POST['Women'] === 'on' && $_POST['Women'] !== '')
+    {
+        echo 'Пол - Женский';  
+    }
 
 
-//if(isset($_POST['Men'] === 'on')){
-   // echo 'Пол - Мужской';
-//}
-//if($women === on){
- //   echo 'Пол - Женский';
-//}
+
 
